f47b69533aec4891d1a51b61304d2f9fc5308c19
Timothy Axford <txa334@case.edu>
2023-03-29 13:30:00 -0400

    Added Values to the Dictionay

--- Dictionary.Net5.0/Program.cs
+++ Dictionary.Net5.0/Program.cs
@@ -1,11 +1,27 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Dictionary.Net5._0
 {
     internal class Program
     {
+
+        // Key - Value
+        // Auto - Car
+
         static void Main(string[] args)
         {
+            Dictionary<int, string> myDictionary = new Dictionary<int, string>()
+            {
+                { 1, "one" },
+                { 2, "two" },
+                { 3, "three" }
+            };
+
+
+
+
+
             Console.WriteLine("Hello World!");
         }
     }
