036fffc62599e5dfa5e610ab870343361f300209
Timothy Axford <txa334@case.edu>
2023-03-29 13:36:07 -0400

    Added new Employee class with some properties, get method for salary and a simple constructor

--- Dictionary.Net5.0/Program.cs
+++ Dictionary.Net5.0/Program.cs
@@ -25,4 +25,32 @@ namespace Dictionary.Net5._0
             Console.WriteLine("Hello World!");
         }
     }
+
+    class Employee
+    {
+        //few properties like Role Name, Ahe and Rate
+
+        public string Role { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public float Rate { get; set; }
+        //Yearly Salary = rate/h * number of days * number of weeks * number of months
+
+        public float Salary
+        {
+            get
+            {
+                return Rate * 8 * 5 * 4 * 12;
+            }
+        }
+        //simple constructor
+        public Employee(string role, string name, int age)
+        {
+            this.Role = role;
+            this.Name = name;
+            this.Age = age;
+            this.Rate = Rate;
+        }
+    }
+
 }
